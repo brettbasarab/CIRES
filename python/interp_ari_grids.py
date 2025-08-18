ce592256bdb40a2f643999cc40dca1f3c1fb29e3
Brett Basarab <brettbasarab@gmail.com>
2025-08-18 16:55:09 -0600

    Updated interp_ari_grids.py to be able to regrid ARIs to AORC grid; updated plot_ari_exceedances.py to plot ARI exceedances for high-res datasets (Nested Replay, etc.) on AORC grid; deleted plot_ari_grids.py, a short script with easily-reproducible ARI plotting (ARI plotting now included in interp_ari_grids.py)

--- python/interp_ari_grids.py
+++ python/interp_ari_grids.py
@@ -7,10 +7,13 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import precip_data_processors as pdp
 import precip_plotting_utilities as pputils
 import sys
 import utilities as utils
 import xarray as xr
+
+# TODO: Add ability to interpolate using xesmf (in addition to current cdo capability)
 
 def main():
     parser = argparse.ArgumentParser(description = "Interpolate ARI data from Stage IV grid using cdo command line utility")
@@ -18,51 +21,89 @@ def main():
                         help = "ARI; default 2 years")
     parser.add_argument("--duration", dest = "duration", type = int, default = 24,
                         help = "Duration; default 24 hours")
+    parser.add_argument("--interp_engine", dest = "interp_engine", default = "cdo", choices = ["cdo", "xesmf"],
+                        help = "Interpolation engine to use; choices are cdo or xesmf")
     parser.add_argument("-i", "--interp_type", dest = "interp_type", default = "bilinear",
                         help = "CDO interpolation type to use; default bilinear")
-    parser.add_argument("-d", "--dest_grid", dest = "dest_grid", default = "Replay", choices = ["Replay", "0.1deg", "0.25deg"],
+    parser.add_argument("-d", "--dest_grid", dest = "dest_grid", default = "Replay", choices = ["Replay", "AORC", "0.1deg", "0.25deg"],
                         help = "Destination grid type; default Global Replay grid")
+    parser.add_argument("-p", "--plot", dest = "plot", default = False, action = "store_true",
+                        help = "Set to plot original and interpolated ARI grid")
     args = parser.parse_args()
 
     # Selected Replay data already output to netCDF to use as template file for interpolation
     nc_dir = "/data/bbasarab/netcdf"
-    template_fpath = os.path.join(nc_dir, "TemplateGrids", "ReplayGrid.nc")
+    output_grid_string = pdp.set_grid_name_for_file_names(args.dest_grid)
+    template_fpath = os.path.join(nc_dir, "TemplateGrids", f"{output_grid_string}.nc")
 
-    # Set interpolation type to be used by cdo
-    cdo_interp_type = utils.set_cdo_interpolation_type(args.interp_type) 
+    if (args.interp_engine == "cdo"):
+        # Set interpolation type to be used by cdo
+        cdo_interp_type = utils.set_cdo_interpolation_type(args.interp_type) 
 
-    # Configure input file path
-    input_fpath = determine_ari_input_fpath(args.ari, args.duration) 
+        # Configure input file path
+        input_fpath = determine_ari_input_fpath(args.ari, args.duration) 
 
-    # Configure output file path
-    output_dir = os.path.join(nc_dir, f"ARIs.{args.dest_grid}Grid")
-    output_file = f"ARI.{args.dest_grid}Grid.{args.ari:02d}_year.{args.duration:02d}_hour_precipitation.nc"
-    output_fpath = os.path.join(output_dir, output_file)
+        # Configure output file path
+        output_dir = os.path.join(nc_dir, f"ARIs.{output_grid_string}")
+        output_file = f"ARI.{output_grid_string}.{args.ari:02d}_year.{args.duration:02d}_hour_precipitation.nc"
+        output_fpath = os.path.join(output_dir, output_file)
 
-    # Configure cdo interpolation command
-    # Regrid CONUS404 to 0.1 degree rectilinear grid
-    if (args.dest_grid == "0.1deg"):
-        cdo_cmd = f"cdo -P 8 {cdo_interp_type},global_0.1 {input_fpath} {output_fpath}"
-        plot_name = "0p1deg_grid"
-        print("**** Interpolating to 0.1 degree grid")
-    # Regrid CONUS404 to 0.25 degree rectilinear grid
-    elif (args.dest_grid == "0.25deg"):
-        cdo_cmd = f"cdo -P 8 {cdo_interp_type},global_0.25 {input_fpath} {output_fpath}"
-        plot_name = "0p25deg_grid" 
-        print("**** Interpolating to 0.25 degree grid")
-    # Regrid CONUS404 to Replay grid, using Replay data itself as the template
+        # Configure CDO interpolation command
+        cdo_cmd = configure_cdo_interp_command(output_grid_string, cdo_interp_type, template_fpath,
+                                               input_fpath, output_fpath)
+        # Run cdo command
+        print(f"Running: {cdo_cmd}")
+        ret = os.system(cdo_cmd)
+        if (ret != 0):
+            print("Error: cdo interpolation command did not work")
+            sys.exit(1)
     else:
-        cdo_cmd = f"cdo -P 8 {cdo_interp_type},{template_fpath} {input_fpath} {output_fpath}"
-        plot_name = "ReplayGrid"
-        print("**** Interpolating to Replay grid")
+        print(f"Error: ARI grid interpolation with xesmf not yet implemented")
+        sys.exit(0)
 
-    # Run cdo command
-    print(f"Running: {cdo_cmd}")
-    ret = os.system(cdo_cmd)
-    if (ret != 0):
-        print("Error: cdo interpolation command did not work")
-        sys.exit(1)
-        
+    if args.plot:
+        print("Plotting")
+
+        # ARIs on original (native) grid
+        input_da = xr.open_dataset(input_fpath).precip
+        pputils.plot_cmap_single_panel(input_da,
+                                       f"ARI.{args.ari:02d}yr.{args.duration:02d}hr.NativeGrid",
+                                       f"ARI.{args.ari:02d}yr.{args.duration:02d}hr.NativeGrid",
+                                       "CONUS",
+                                       plot_levels = np.arange(0, 210, 10))
+
+        # ARIs on output grid
+        output_da = xr.open_dataset(output_fpath).precip
+        pputils.plot_cmap_single_panel(output_da,
+                                       f"ARI.{args.ari:02d}yr.{args.duration:02d}hr.{output_grid_string}",
+                                       f"ARI.{args.ari:02d}yr.{args.duration:02d}hr.{output_grid_string}",
+                                       "CONUS",
+                                       plot_levels = np.arange(0, 210, 10))
+
+def configure_cdo_interp_command(dest_grid_string, cdo_interp_type, template_fpath, input_fpath, output_fpath):
+    match dest_grid_string:
+        # Regrid to 0.1 degree rectilinear grid
+        case "0.1deg":
+            print("**** Interpolating to 0.1 degree grid")
+            cdo_cmd = f"cdo -P 8 {cdo_interp_type},global_0.1 {input_fpath} {output_fpath}"
+        # Regrid to 0.25 degree rectilinear grid
+        case "0.25deg":
+            print("**** Interpolating to 0.25 degree grid")
+            cdo_cmd = f"cdo -P 8 {cdo_interp_type},global_0.25 {input_fpath} {output_fpath}"
+        # Regrid to Replay grid, using Replay data itself as the template
+        case "ReplayGrid":
+            print("**** Interpolating to Replay grid")
+            cdo_cmd = f"cdo -P 8 {cdo_interp_type},{template_fpath} {input_fpath} {output_fpath}"
+        # Regrid to AORC grid, using AORC data itself as the template
+        case "AorcGrid":
+            print("**** Interpolating to AORC grid")
+            cdo_cmd = f"cdo -P 8 {cdo_interp_type},{template_fpath} {input_fpath} {output_fpath}"
+        case _:
+            print(f"Error: Interpolation to {dest_grid_string} grid not implemented")
+            sys.exit(1)
+
+    return cdo_cmd
+ 
 def determine_ari_input_fpath(ari, duration):
     input_dir = "/Projects/WPC_ExtremeQPF/newARIs/"
     input_file = f"allusa_ari_{ari}yr_{duration}hr_xarray_st4grid.nc"
--- python/plot_ari_exceedances.py
+++ python/plot_ari_exceedances.py
@@ -18,6 +18,12 @@ def main():
                         help = "Start date/time of verification; format consistent with temporal_res flag, e.g., 'YYYYmmdd' for 24-hourly data")
     parser.add_argument("end_dt_str",
                         help = "End date/time of verification; format consistent with temporal_res flag, e.g., 'YYYYmmdd' for 24-hourly data")
+    parser.add_argument("--data_names_list", dest = "data_names_list", default = None, nargs = "+",
+                        help = "List of data names to verify; pass space-separated with the truth data name listed first; if None, all possible datasets for the given region are verified")
+    parser.add_argument("--verif_grid", dest = "verif_grid", default = "Replay", choices = ["AORC", "Replay"],
+                        help = "Set a standard verification grid for CONUS (or subregions) verification [AORC and (global) Replay are currently supported; default Replay]")
+    parser.add_argument("--include_hrrr", dest = "include_hrrr", action = "store_true", default = False,
+                        help = "Set to include HRRR in verification")
     parser.add_argument("--region", dest = "region", default = "CONUS",
                         help = "Region to zoom plot to; default CONUS")
     parser.add_argument("--temporal_res", dest = "temporal_res", default = 24, type = int, choices = [1, 3, 24],
@@ -25,9 +31,28 @@ def main():
     parser.add_argument("--ari", dest = "ari", type = int, default = 2,
                         help = "Average Recurrence Interval (ARI) for which to count exceedances; default 2-year") 
     args = parser.parse_args()
+    
+    # Read ARI grid
+    output_grid_string = pdp.set_grid_name_for_file_names(args.verif_grid)
+    ari_dir = os.path.join(utils.data_nc_dir, f"ARIs.{output_grid_string}")
+    ari_fname = f"ARI.{output_grid_string}.{args.ari:02d}_year.{args.temporal_res}_hour_precipitation.nc"
+    ari_fpath = os.path.join(ari_dir, ari_fname)
+    print(f"Reading {ari_fpath}")
+    if not (os.path.exists(ari_fpath)):
+        print(f"Error: ARI file {ari_fpath} does not exist")
+        sys.exit(1)
+    aris = xr.open_dataset(ari_fpath).precip
 
-    data_names, truth_data_name, data_grid = precip_verification_processor.map_region_to_data_names(args.region)
+    # Determine data names to use for this particular type of verification 
+    if (type(args.data_names_list) is list) and (len(args.data_names_list) >= 1):
+        data_names, truth_data_name = args.data_names_list, args.data_names_list[0]
+        _, _, data_grid = precip_verification_processor.map_region_to_data_names(args.region, verif_grid = args.verif_grid)
+    else:
+        data_names, truth_data_name, data_grid = precip_verification_processor.map_region_to_data_names(args.region,
+                                                                                                        verif_grid = args.verif_grid,
+                                                                                                        include_hrrr = args.include_hrrr)
 
+    # Instantiate PrecipVerificationProcessor class
     verif = precip_verification_processor.PrecipVerificationProcessor(args.start_dt_str, 
                                                                       args.end_dt_str,
                                                                       data_names = data_names, 
@@ -36,19 +61,13 @@ def main():
                                                                       region = args.region,
                                                                       temporal_res = args.temporal_res)
 
-
-    ari_dir = os.path.join(utils.data_nc_dir, "ARIs.ReplayGrid")
-    ari_fname = f"ARI.ReplayGrid.{args.ari:02d}_year.{args.temporal_res}_hour_precipitation.nc"
-    ari_fpath = os.path.join(ari_dir, ari_fname)
-    if not (os.path.exists(ari_fpath)):
-        print(f"Error: ARI file {ari_fpath} does not exist")
-        sys.exit(1)
-    aris = xr.open_dataset(ari_fpath).precip
+    # Subset ARI grid to current region
     aris_region_subset = verif._subset_data_to_region(aris, data_name = "ARI_grid")
 
+    # Calculate number of ARI exceedances
     ari_exceedance_dict = {}
     for data_name, da in verif.da_dict.items():
-        # Calculate ARI exceedances, sum over time period
+        # Calculate ARI exceedances, sum over full time period along time dimension
         ari_exceedance_da = xr.where(da > aris_region_subset, 1, 0, keep_attrs = True)
         ari_exceedance_da_sum = ari_exceedance_da.sum(dim = utils.period_end_time_dim_str, keepdims = True)
 
@@ -67,7 +86,7 @@ def main():
 
     # Plot cumulative ARI exceedances
     cmap, bounds, norm = create_ari_colorbar()
-    verif.plot_cmap_multi_panel(data_dict = ari_exceedance_dict, plot_levels = bounds, cmap = cmap) 
+    verif.plot_cmap_multi_panel(data_dict = ari_exceedance_dict, plot_levels = bounds, plot_cmap = cmap) 
  
 def create_ari_colorbar():
     color_list = ["white", "cyan", "blue", "green", "red", "purple"]
