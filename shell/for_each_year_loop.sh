8869e554c3229c14fc454fa860d59b383d504af6
Brett Basarab <brettbasarab@gmail.com>
2025-08-20 16:47:50 -0600

    Corrected comment in for_each_year_loop.sh

--- shell/for_each_year_loop.sh
+++ shell/for_each_year_loop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# In a directory (must be in this directory), count number of files for each specified year (currently 2002-2024)
+# In a directory (must be in this directory), count number of files for each specified year (currently 2002-2021)
 for year in 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21; do echo "******** YEAR: 20${year}"; ls *.20${year}*.nc | wc -l; done
